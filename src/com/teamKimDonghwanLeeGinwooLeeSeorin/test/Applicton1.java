f23425221477c563ec8667634ec37c091825eeed
lsl0217 <0217gir@gmail.com>
2023-12-29 17:52:20 +0900

    first commit

---
+++ src/com/teamKimDonghwanLeeGinwooLeeSeorin/test/Applicton1.java
@@ -0,0 +1,7 @@
+package com.teamKimDonghwanLeeGinwooLeeSeorin.test;
+
+public class Applicton1 {
+    public static void main(String[] args) {
+        System.out.println("git 나에겐 너무 헷갈리는");
+    }
+}
